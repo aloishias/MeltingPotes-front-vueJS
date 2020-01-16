3891b0121807ae099e200791d7f3c193c494815e
Aloïs <aloiiiiiiis@gmail.com>
2020-01-16 17:45:18 +0100

    Le commit nul

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,14 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/layout/Header.vue'
+import Footer from './components/layout/Footer.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Footer
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/router.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+/*
+import Home from './views/Home.vue'
+import Inscription from "./views/Inscription.vue"
+import RestaurantList from './views/RestaurantList'
+*/
+
+Vue.use(Router)
+
+export default new Router({
+    /*
+    routes: [
+        {
+            path: '/',
+            name: 'timeline',
+            component: timeline
+        },
+        {
+            path: '/profil',
+            name: 'profil',
+            component: profil,
+        }
+    ]
+    */
+})
